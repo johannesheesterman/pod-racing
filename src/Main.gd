3cfff77f314a9d7a2f07d01e21a118565d9a6c17
Johannes Heesterman <johannes@elfsquad.io>
2022-08-29 20:33:08 +0200

    initialize player ships at correct starting position

--- src/Main.gd
+++ src/Main.gd
@@ -4,11 +4,12 @@ func initialize_players():
 	var p1Scene = preload("res://Player1Character.tscn")
 	var p2Scene = preload("res://Player2Character.tscn")
 
-	for _i in range(3):
+	for i in range(3):
 		var p1Instance = p1Scene.instance()
 		var p2Instance = p2Scene.instance()
 		
-		p1Instance.transform.basis = Basis()	
+		p1Instance.transform.origin = Vector3(-1 + i, 0, 2)
+		p2Instance.transform.origin = Vector3(-2, 0, -1 + i)
 			
 		add_child(p1Instance)
 		add_child(p2Instance)
